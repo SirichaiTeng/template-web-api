f20db779bba092dfac51c6ab93cff5322329cb47
Sirichai <chainis.teon@gmail.com>
2025-07-09 00:03:10 +0700

    Initial project template web api s,m

---
+++ template-web-api/src/TemplateM/template-web-api-M/template-web-api-M.Bussiness/template-web-api-M.Bussiness/Common/ConnectionFactory/DbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_web_api_M.Bussiness.Common.ConnectionFactory;
+internal class DbContext
+{
+}
