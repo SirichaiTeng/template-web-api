7aa592ad654cb42d32a155e003980c59d1324b08
Sirichai <chainis.teon@gmail.com>
2026-01-19 00:12:21 +0700

    Ad Utils & ApiResponse

---
+++ template-web-api/src/OriginalExample/Utils/FileUtil.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace OriginalExample.Utils;
+
+public static class FileUtil
+{
+    public static async Task<T?> ReadJsonFileAsync<T>(string filePath)
+    {
+        using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        {
+            return await JsonSerializer.DeserializeAsync<T>(fs, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true, IncludeFields = true});
+        }
+    }
+
+    public static async Task<string> ReadTextFileAsync(string filePath)
+    {
+        using FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+        using StreamReader reader = new StreamReader(fs);
+        return await reader.ReadToEndAsync();
+    }
+}
